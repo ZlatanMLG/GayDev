66155791c79734982054918b8922ea0b77644cb6
HumbleShark <66707106+HumbleShark@users.noreply.github.com>
2020-07-28 14:56:39 +0300

    Create CollisionDetection.cs

---
+++ Scripts/CollisionDetection.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetection : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        print("Pickup detected!");
+        Destroy(gameObject);
+    }
+}
